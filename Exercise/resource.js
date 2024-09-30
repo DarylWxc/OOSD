a842959c2a9b6ade23d147c710b8b7a44cd7a7e5
DarylWxc <darylWxc@outlook.com>
2024-09-29 21:32:24 -0600

    # Commit Daily Assignments for My Repository

---
+++ Exercise/resource.js
@@ -0,0 +1,55 @@
+/* Author: Daryl
+Date: September 29 2024
+Course Module: CPRG210
+Assignment: HTML CSS JS Assignment */
+
+const websites = [
+    "https://en.wikipedia.org/wiki/Aurora",
+    "https://www.quora.com/How-does-winter-in-Canada-looks-like",
+    "https://www.travelandleisure.com/trip-ideas/beach-vacations/most-beautiful-beaches-in-the-world",
+]
+const imageArray = [
+    {
+      src: "image/travel\ picture1.jpg",
+    },
+    {
+      src: "image/travel\ picture2.jpg",
+    },
+    {
+      src: "image/travel\ picture3.png",
+    },
+  ];
+  const stringArray = [
+    "This is the first Background",
+    "This is the second Background",
+    "This is the third Background",
+  ];
+
+  const provinces = [
+    "AB",
+    "Alberta",
+    "BC",
+    "British Columbia",
+    "MB",
+    "Manitoba",
+    "NB",
+    "New Brunswick",
+    "NL",
+    "Newfoundland",
+    "NS",
+    "Nova Scotia",
+    "NT",
+    "Northwest Territories",
+    "NU",
+    "Nunavut",
+    "ON",
+    "Ontario",
+    "PE",
+    "Prince Edward Island",
+    "QC",
+    "Quebec",
+    "SK",
+    "Saskatchewan",
+    "YT",
+    "Yukon",
+  ];
